35db13b6c708c8fc8a9f70eaa6816f9fed49350c
Brendan Irvine-Broque <birvine-broque@cloudflare.com>
2025-03-19 18:25:32 -0700

    Basic example of Braintrust in Worker wrapping OpenAI

---
+++ README.md
@@ -0,0 +1,6 @@
+
+```
+}
+[wrangler:inf] GET / 200 OK (190ms)
+✘ [ERROR] Unsupported OpenAI library (potentially v3). Not wrapping.
+```
--- src/index.ts
+++ src/index.ts
@@ -1,18 +1,38 @@
-/**
- * Welcome to Cloudflare Workers! This is your first worker.
- *
- * - Run `npm run dev` in your terminal to start a development server
- * - Open a browser tab at http://localhost:8787/ to see your worker in action
- * - Run `npm run deploy` to publish your worker
- *
- * Bind resources to your worker in `wrangler.jsonc`. After adding bindings, a type definition for the
- * `Env` object can be regenerated with `npm run cf-typegen`.
- *
- * Learn more at https://developers.cloudflare.com/workers/
- */
+import { initLogger, wrapOpenAI, wrapTraced } from "braintrust";
+
+import {
+  streamText,
+} from "ai";
+import { createOpenAI } from "@ai-sdk/openai";
 
 export default {
 	async fetch(request, env, ctx): Promise<Response> {
+
+		const logger = initLogger({
+			projectName: "My Project",
+			apiKey: env.BRAINTRUST_API_KEY,
+			asyncFlush: false,
+		  });
+
+          const openai = wrapOpenAI(createOpenAI({
+            apiKey: env.OPENAI_API_KEY,
+          }));
+
+          // Stream the AI response using GPT-4
+          const result = streamText({
+            model: openai("gpt-4o-2024-11-20"),
+            system: `You are a helpful assistant that can do various tasks...`,
+            messages: [
+				{
+					role: 'user',
+					content: 'foo'
+				}
+			],
+            maxSteps: 10,
+          });
+
+		  console.log(result);
+
 		return new Response('Hello World!');
 	},
 } satisfies ExportedHandler<Env>;
